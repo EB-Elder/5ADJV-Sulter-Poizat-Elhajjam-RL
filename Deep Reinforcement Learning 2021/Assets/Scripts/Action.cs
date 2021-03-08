3c160541f3e95f28803c3582338521b2509563d1
elhajjam.abdallah1@gmail.com <elhajjam.abdallah1@gmail.com>
2021-03-08 15:35:16 +0100

    Starting !

---
+++ Deep Reinforcement Learning 2021/Assets/Scripts/Action.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action
+{
+
+    public int idAction;
+    public float reward;
+    public State stateDestination;
+}
